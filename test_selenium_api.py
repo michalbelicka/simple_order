8054e38724b79a728116d59d3fa575f3d2eb54c2
michalbelicka <michal.belicka2310@gmail.com>
2025-09-26 15:36:12 +0200

    Delete comment

--- test_selenium_api.py
+++ test_selenium_api.py
@@ -10,7 +10,7 @@ from selenium.webdriver.chrome.options import Options
 def test_selenium_api():
     service = Service(ChromeDriverManager().install())
     options = Options()
-    options.add_argument("--headless=new")  # Headless mód
+    options.add_argument("--headless=new") 
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--incognito")
