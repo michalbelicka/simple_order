06ad0a4ad920f285a96de0e883377570bdd1e225
michalbelicka <michal.belicka2310@gmail.com>
2025-10-17 11:19:29 +0200

    Add standalone PUT test for order

--- test_put_api.py
+++ test_put_api.py
@@ -1,34 +1,65 @@
-# import requests
-# from test_post_fixture import created_order
+import pytest
+import requests
 
-# def test_update_order(created_order):
-#     order_id = created_order
-
-#     update_data = {
-#         "name": "UpdatedUser",
-#         "email": "Updated_user@example.com",
-#         "address": "UpdatedAddress",
-#         "product": "UpdatedProduct",
-#         "quantity": 4
-#     }
-
-#     response = requests.put(f"http://127.0.0.1:5000/api/order/{order_id}", json=update_data)
-#     assert response.status_code == 200
-#     assert response.json()["message"] == "order updated"
-
-#     response_get = requests.get(f"http://127.0.0.1:5000/order/{order_id}")
-#     updated_order = response_get.json()
-
+@pytest.fixture(scope="module")
+def new_order(clear_db):
     
-#     assert updated_order["name"] == "UpdatedUser"
-#     assert updated_order["email"] == "Updated_user@example.com"
-#     assert updated_order["address"] == "UpdatedAddress"
-#     assert updated_order["product"] == "UpdatedProduct"
-#     assert updated_order["quantity"] == 4
+    post_data = {
+        "name": "User",
+        "email": "user@example.com",
+        "address": "Address",
+        "product": "Product",
+        "quantity": 2
+    }
+    response = requests.post("http://127.0.0.1:5000/api/order", json=post_data)
+    assert response.status_code == 201
+    assert response.json()["message"] == "Order created"
+    order_id = response.json()["id"]
+    response_get = requests.get(f"http://127.0.0.1:5000/api/order/{order_id}")
+    order = response_get.json()
 
-#     assert isinstance(updated_order["id"], int)
-#     assert isinstance(updated_order["name"], str)
-#     assert isinstance(updated_order["email"], str)
-#     assert isinstance(updated_order["address"], str)
-#     assert isinstance(updated_order["product"], str)
-#     assert isinstance(updated_order["quantity"], int)
+    assert order["name"] == "User"
+    assert order["email"] == "user@example.com"
+    assert order["address"] == "Address"
+    assert order["product"] == "Product"
+    assert order["quantity"] == 2
+
+    assert isinstance(order["id"], int)
+    assert isinstance(order["name"], str)
+    assert isinstance(order["email"], str)
+    assert isinstance(order["address"], str)
+    assert isinstance(order["product"], str)
+    assert isinstance(order["quantity"], int)
+
+    yield order_id
+    response_del = requests.delete(f"http://127.0.0.1:5000/api/order/{order_id}")
+    assert response_del.status_code == 200
+    assert response_del.json()["message"] == "Order successfully deleted"
+
+def test_put_order(new_order):
+    order_id = new_order
+    put_data = {
+        "name": "PutUser",
+        "email": "put_user@example.com",
+        "address": "PutAddress",
+        "product": "PutProduct",
+        "quantity": 6
+    }
+    response_put = requests.put(f"http://127.0.0.1:5000/api/order/{order_id}", json=put_data)
+    assert response_put.status_code == 200
+    assert response_put.json()["message"] == "order updated"
+    response_get = requests.get(f"http://127.0.0.1:5000/api/order/{order_id}")
+    updated_order = response_get.json()
+
+    assert updated_order["name"] == "PutUser"
+    assert updated_order["email"] == "put_user@example.com"
+    assert updated_order["address"] == "PutAddress"
+    assert updated_order["product"] == "PutProduct"
+    assert updated_order["quantity"] == 6
+
+    assert isinstance(updated_order["id"], int)
+    assert isinstance(updated_order["name"], str)
+    assert isinstance(updated_order["email"], str)
+    assert isinstance(updated_order["address"], str)
+    assert isinstance(updated_order["product"], str)
+    assert isinstance(updated_order["quantity"], int)
