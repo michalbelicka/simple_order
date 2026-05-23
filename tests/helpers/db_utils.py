c92c81c74bda29bc12a20ebf18e6efc777ca58c8
michalbelicka <michal.belicka2310@gmail.com>
2026-05-23 16:21:11 +0200

    Update DB path handling

--- tests/helpers/db_utils.py
+++ tests/helpers/db_utils.py
@@ -2,6 +2,7 @@ import sqlite3
 import os
 
 def get_order_from_db(order_id):
+    
     db_path = os.path.join(os.path.dirname(__file__ ), "..", "..", "orders.db")
     conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
