739d86261db8705b60bf92800d97e77d9cc6cbb9
michalbelicka <michal.belicka2310@gmail.com>
2026-06-03 19:29:17 +0200

    Add delete test api

---
+++ tests/test_delete_api.py
@@ -0,0 +1,41 @@
+from tests.helpers.db_utils import get_order_from_db
+
+def test_delete_order(client):
+    # ==================================================
+    # ARRANGE - CREATE
+    # ==================================================
+    post_data = {
+        "name": "User",
+        "email": "user@example.com",
+        "address": "Address",
+        "product": "Product",
+        "quantity": 2
+    }
+
+    res = client.post("/api/order", json=post_data)
+
+    assert res.status_code == 201
+
+    order_id = res.get_json()["id"]
+
+    # ==================================================
+    # ACT - DELETE
+    # ==================================================
+    res = client.delete(f"/api/order/{order_id}")
+
+    # ==================================================
+    # ASSERT - DELETE (API)
+    # ==================================================
+    assert res.status_code == 200
+    assert res.get_json()["message"] == "Order successfully deleted"
+
+    res = client.get(f"/api/order/{order_id}")
+
+    assert res.status_code == 404
+
+    # ==================================================
+    # ASSERT - DELETE (DB)
+    # ==================================================
+    row = get_order_from_db(order_id)
+
+    assert row is None
