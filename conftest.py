080a34c31c69acfd042bdf2d728965c8b1aa1803
michalbelicka <michal.belicka2310@gmail.com>
2026-06-03 16:42:33 +0200

    feat: add pytest client fixture with test database setup

--- conftest.py
+++ conftest.py
@@ -1,11 +1,33 @@
 import pytest
 from app import app
 from models import db, Order
+from pathlib import Path
 
-@pytest.fixture(scope="module", autouse=True)
-def clear_db():
+# @pytest.fixture(scope="module", autouse=True)
+# def clear_db():
+
+#     with app.app_context():
+#         db.session.query(Order).delete()
+#         db.session.commit()
+
+BASE_DIR = Path(__file__).resolve().parent
+db_path = BASE_DIR / "tests" / "test.db"
+
+@pytest.fixture
+def client():
+
+    app.config["TESTING"] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
 
     with app.app_context():
-        db.session.query(Order).delete()
-        db.session.commit()
+        db.drop_all()
+        db.create_all()
+    
+    with app.test_client() as client:
+        yield client
+    
+    with app.app_context():
+        db.session.remove()
+        db.drop_all()
+
 
