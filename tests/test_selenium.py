734441eac5278104b9a418d204176af08a5aadc8
michalbelicka <michal.belicka2310@gmail.com>
2026-06-04 16:21:53 +0200

    fix flaky selenium test caused by render cold start

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -17,7 +17,7 @@ def test_selenium_api():
     options.add_argument("--incognito")
 
     driver = webdriver.Chrome(service=service, options=options)
-    wait = WebDriverWait(driver, 30)
+    wait = WebDriverWait(driver, 60)
 
     driver.get("https://belicka-orders-api.onrender.com/")
 
